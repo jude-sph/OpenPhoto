6ca60c3168b812ec16102f3c6e08df1714970379
jude-sph <49697990+jude-sph@users.noreply.github.com>
2026-06-08 00:00:04 +0100

    fix: restore swift-tools-version 6.0 and platforms macOS 15
    The 5.9 downgrade was a casualty of debugging the CLT orphan-file
    issue (stale module.modulemap + private.swiftinterface files from
    old CLT versions broke manifest compilation machine-wide; fixed by
    deleting unowned files identified via pkgutil).

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "OpenPhoto",
+    platforms: [.macOS(.v15)],
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
     ],
